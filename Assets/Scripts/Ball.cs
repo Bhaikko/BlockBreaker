492ddce947b613d99846711a0f68d5febafdad6b
Siddharth Pawar <siddhartha.pawar17@gmail.com>
2020-04-16 13:48:04 +0530

    Audio Clips Added

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -9,13 +9,19 @@ public class Ball : MonoBehaviour
     [SerializeField] float xVelocity = 2.0f;
     [SerializeField] float yVelocity = 15.0f;
 
+    [SerializeField] AudioClip[] ballSounds = null;
+
     // State Parameters
     Vector2 paddleToBallVector = new Vector2();
     bool hasStarted = false;
 
+    // Cached Component References
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         paddleToBallVector = this.transform.position - paddle.transform.position;
     }
 
@@ -43,6 +49,14 @@ public class Ball : MonoBehaviour
             hasStarted = true;
             // Add velocity to rigidbody component of this component
             GetComponent<Rigidbody2D>().velocity = new Vector2(xVelocity, yVelocity); 
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted) {
+            AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
+            audioSource.PlayOneShot(clip);
         }
     }
 }
--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound = null;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, transform.position);
+
         // gameObject refers to this game object
         Destroy(gameObject);
     }
