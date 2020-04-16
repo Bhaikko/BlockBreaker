8718e832e1d0b234e22bc8360e93e3a7af2f43ef
Siddharth Pawar <siddhartha.pawar17@gmail.com>
2020-04-16 12:32:22 +0530

    Blocks Destroyable Upon Collision

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // gameObject refers to this game object
+        Destroy(gameObject, 2);
+    }
+}
