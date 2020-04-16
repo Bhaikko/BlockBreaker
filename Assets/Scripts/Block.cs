62593b7c6d179951445fffea72076d234a5fc614
Siddharth Pawar <siddhartha.pawar17@gmail.com>
2020-04-16 14:05:15 +0530

    Blocks Countable

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,11 +6,22 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound = null;
 
+    // Cached Reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.AddBlock();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        AudioSource.PlayClipAtPoint(breakSound, transform.position);
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
 
         // gameObject refers to this game object
         Destroy(gameObject);
+        level.BlockBroke();
+        
     }
 }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] SceneLoader sceneLoader = null;
+
+    int blocksLeft = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Done using Rick's way
+        //blocksLeft = FindObjectsOfType<Block>().Length;
+    }
+
+    public void AddBlock()
+    {
+        blocksLeft++;
+    }
+
+    public void BlockBroke()
+    {
+        blocksLeft--;
+        if (blocksLeft <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
